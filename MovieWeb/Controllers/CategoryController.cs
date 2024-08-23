c178e4d493794a36756cf4b0062d2b0b3d958527
Khai Bui <khaibui0402@gmail.com>
2024-08-23 14:42:08 +0700

    UpdateAdd

---
+++ MovieWeb/Controllers/CategoryController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieWeb.Data;
+using MovieWeb.Models;
+
+namespace MovieWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public CategoryController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _dbContext.Categories.ToList();
+            return View(objCategoryList);
+        }
+
+        // create a new
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+            //  check the fied input is  valid
+            if (ModelState.IsValid)
+            {
+                _dbContext.Categories.Add(category);
+                _dbContext.SaveChanges();
+                return RedirectToAction("Index");  //  you  can chang the action  of index or controller  here
+            }
+            return View();
+        }
+    }
+}
