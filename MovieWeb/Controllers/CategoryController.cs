8290ef88b4817e21f661e52bc74406446ab2ccff
Khai Bui <khaibui0402@gmail.com>
2024-08-27 02:07:28 +0700

    finish CRUD

--- MovieWeb/Controllers/CategoryController.cs
+++ MovieWeb/Controllers/CategoryController.cs
@@ -26,6 +26,11 @@ namespace MovieWeb.Controllers
         [HttpPost]
         public IActionResult Create(Category category)
         {
+            if (category.Name == category.DisplayOrder.ToString())
+            {
+                //  if the name = =  the  display
+                ModelState.AddModelError("Name", "The Display Order does  not match the Name");
+            }
             //  check the fied input is  valid
             if (ModelState.IsValid)
             {
@@ -34,6 +39,70 @@ namespace MovieWeb.Controllers
                 return RedirectToAction("Index");  //  you  can chang the action  of index or controller  here
             }
             return View();
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            // load the database of Sql server
+            //Category? categoryFromDb = _dbContext.Categories.FirstOrDefault(c => c.Id == id);
+            //Category? categoryFromDb = _dbContext.Categories.Where(u => u.Id == id).FirstOrDefault();
+            Category? categoryFromDb = _dbContext.Categories.Find(id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryFromDb);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            if (category.Name == category.DisplayOrder.ToString())
+            {
+                //  if the name = =  the  display
+                ModelState.AddModelError("Name", "The Display Order does  not match the Name");
+            }
+            //  check the fied input is  valid
+            if (ModelState.IsValid)
+            {
+                _dbContext.Categories.Update(category);
+                _dbContext.SaveChanges();
+                return RedirectToAction("Index");  //  you  can chang the action  of index or controller  here
+            }
+            return View();
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            Category? categoryFromDb = _dbContext.Categories.Find(id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryFromDb);
+        }
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteDb(int? id)
+        {
+            Category? obj = _dbContext.Categories.Find(id);
+            if (id == null)
+            { return NotFound(); }
+
+            _dbContext.Categories.Remove(obj); // delete the obj
+            _dbContext.SaveChanges();
+            return RedirectToAction("Index");  //  you  can chang the action  of index or controller  here
+
+            
         }
     }
 }
